22162e1e44e730ec4c2962d1d5ee17bc7c9fc56b
Gr1ff1thIII <dima.a.chugunov@mail.ru>
2024-06-07 03:20:11 +0400

    added tests for finding and updating courses

--- src/course/course.service.spec.ts
+++ src/course/course.service.spec.ts
@@ -2,36 +2,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CourseService } from './course.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaClient } from '@prisma/client';
-import { mockDeep, DeepMockProxy } from 'jest-mock-extended'
 
+const prismaMock = {
+  user: {
+    findFirst: jest.fn(),
+  },
+  course: {
+    create: jest.fn(),
+    findMany: jest.fn(),
+    update: jest.fn(),
+  },
+};
 
 const jwtMock = {
-  decode: jest.fn()
+  decode: jest.fn(),
 };
 
 describe('CourseService', () => {
   let service: CourseService;
-  let prismaMock: DeepMockProxy<PrismaClient>;
-
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        CourseService, 
+        CourseService,
         {
-          provide: PrismaService, 
-          useValue: mockDeep<PrismaClient>(),
+          provide: PrismaService,
+          useValue: prismaMock,
         },
         {
           provide: JwtService,
           useValue: jwtMock,
-        }
+        },
       ],
     }).compile();
 
     service = module.get<CourseService>(CourseService);
 
+    prismaMock.user.findFirst.mockClear();
+    prismaMock.course.create.mockClear();
+    prismaMock.course.findMany.mockClear();
   });
 
   it('should create course and return it', async () => {
@@ -39,8 +48,8 @@ describe('CourseService', () => {
       id: '1',
       email: 'robert_polson@mail.ru',
       password: 'griffith',
-      roles: ['ADMIN']
-    }
+      roles: ['ADMIN'],
+    };
 
     const createdCourse = {
       id: "string",
@@ -49,18 +58,17 @@ describe('CourseService', () => {
       endDate: "string",
       creatorId: user.id,
       prerequisiteCompetencies: [],
-      improvingCompetencies: []
-    }
+      improvingCompetencies: [],
+    };
     prismaMock.course.create.mockResolvedValue(createdCourse);
     prismaMock.user.findFirst.mockResolvedValue(user);
     jwtMock.decode.mockResolvedValue(user);
 
-    const objForCreatingCourse ={
-      name: 'course1'
-    }
+    const objForCreatingCourse = {
+      name: 'course1',
+    };
 
     const result = await service.create(objForCreatingCourse, 'sfa');
-
 
     expect(result).toEqual(createdCourse);
     expect(prismaMock.course.create).toHaveBeenCalledTimes(1);
@@ -85,4 +93,143 @@ describe('CourseService', () => {
       },
     });
   });
-});
+
+  it('should return courses', async () => {
+    const courses = [
+      {
+        id: "string",
+        name: "course1",
+        startDate: "string",
+        endDate: "string",
+        creatorId: '1',
+        prerequisiteCompetencies: [],
+        improvingCompetencies: [],
+      },
+      {
+        id: "string",
+        name: "course2",
+        startDate: "string",
+        endDate: "string",
+        creatorId: '2',
+        prerequisiteCompetencies: [],
+        improvingCompetencies: [],
+      },
+    ];
+
+    prismaMock.course.findMany.mockResolvedValue(courses);
+
+    const result = await service.findAll();
+
+    expect(result).toEqual(courses);
+    expect(prismaMock.course.findMany).toHaveBeenCalledTimes(1);
+    expect(prismaMock.course.findMany).toHaveBeenCalledWith({
+      include: {
+        improvingCompetencies: {
+          include: {
+            competence: true,
+          },
+        },
+        prerequisiteCompetencies: {
+          include: {
+            competence: true,
+          },
+        },
+      },
+    });
+  });
+
+  it('should return all courses for user', async () => {
+    const user = {
+      id: '1',
+      email: 'robert_polson@mail.ru',
+      password: 'griffith',
+      roles: ['ADMIN'],
+    };
+    const courses = [
+      {
+        id: "course1",
+        name: "course1",
+        startDate: "string",
+        endDate: "string",
+        creatorId: user.id,
+        prerequisiteCompetencies: [],
+        improvingCompetencies: [],
+      }
+    ]
+    prismaMock.user.findFirst.mockResolvedValue(user);
+    prismaMock.course.findMany.mockResolvedValue(courses);
+    jwtMock.decode.mockResolvedValue(user);
+
+    const result = await service.findAllForUser('sfa');
+    expect(result).toEqual(courses);
+    expect(prismaMock.course.findMany).toHaveBeenCalledTimes(1);
+    expect(prismaMock.course.findMany).toHaveBeenCalledWith({
+      where: {
+        OR: [
+          {
+            creatorId: user.id,
+          },
+          {
+            students: {
+              some: {
+                userId: user.id,
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        improvingCompetencies: {
+          include: {
+            competence: true,
+          },
+        },
+        prerequisiteCompetencies: {
+          include: {
+            competence: true,
+          },
+        },
+      },
+    })
+  });
+
+  it('should update course', async () => {
+    const courseId = 'courseId1';
+    const updateCourseDto = { name: 'course1', studentId: 'student1' };
+    const updatedCourse = {
+      id: "string",
+      name: courseId,
+      startDate: "string",
+      endDate: "string",
+      creatorId: "1",
+      prerequisiteCompetencies: [],
+      improvingCompetencies: []
+    };
+    prismaMock.course.update.mockResolvedValue(updatedCourse);
+    const result = await service.update(courseId, updateCourseDto);
+
+    expect(result).toEqual(updatedCourse);
+    expect(prismaMock.course.update).toHaveBeenCalledTimes(1);
+    expect(prismaMock.course.update).toHaveBeenCalledWith({
+      where: {
+        id: courseId,
+      },
+      data: {
+        name:updateCourseDto.name,
+        students:{ connect: { id: updateCourseDto.studentId } },
+      },
+      include: {
+        improvingCompetencies: {
+          include: {
+            competence: true,
+          },
+        },
+        prerequisiteCompetencies: {
+          include: {
+            competence: true,
+          },
+        },
+      },
+    });
+  });
+}); 
--- src/student/student.controller.spec.ts
+++ src/student/student.controller.spec.ts
@@ -1,43 +1,32 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { StudentController } from './student.controller';
-// import { StudentService } from './student.service';
-// import { PrismaModule } from '../prisma/prisma.module';
-// import { GetStudentsResponseDto } from './dto/res-get-students.dto';
+import { Test, TestingModule } from '@nestjs/testing';
+import { StudentController } from './student.controller';
+import { StudentService } from './student.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { GetStudentsResponseDto } from './dto/res-get-students.dto';
 
-// describe('StudentController', () => {
-//   let controller: StudentController;
-//   let service: StudentService;
+describe('StudentController', () => {
+  let controller: StudentController;
+  let service: StudentService;
 
-//   const mockStudentService={
-//     findStudents: jest.fn(),
-//     updateStudent: jest.fn(),
-//   };
+  const mockStudentService={
+    findStudents: jest.fn(),
+    updateStudent: jest.fn(),
+  };
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       imports: [PrismaModule],
-//       controllers: [StudentController],
-//       providers: [StudentService],
-//     }).overrideProvider(StudentService).useValue(mockStudentService).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
+      controllers: [StudentController],
+      providers: [StudentService],
+    }).overrideProvider(StudentService).useValue(mockStudentService).compile();
 
-//     controller = module.get<StudentController>(StudentController);
-//     service = module.get<StudentService>(StudentService);
-//   });
+    controller = module.get<StudentController>(StudentController);
+    service = module.get<StudentService>(StudentService);
+  });
 
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
 
-//   describe('findStudentForSearch', () =>{
-//     it ('should return students', async () =>{
-//       const result: GetStudentsResponseDto={
-//         students:[{
-//           id:'1',
-//           email:'soski@mail.ru',
-//           firstName:
-//         },
-//         ],
-//       }
-//       expect(controller.findStudentForSearch).to
-//     })
-// });
+  
+});
